eefb89efc293336231ad79436295d88a9a56c240
shaikat17 <shaikatpal56@gmail.com>
2024-10-26 12:15:31 +0600

    Make some changes on meal sharing button.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,11 @@
 import ImagePicker from '@/components/meals/image-picker';
+
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+
   
   return (
     <>
@@ -43,7 +46,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Select Recipe Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
